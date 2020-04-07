e038d5f9d59caca7aa0b2057dc679271c19d8da3
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 20:47:47 +0000

    3.4.1

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/7/2020
+//asks for the user's name and displays a welcome message using their name
 package main
 
 import "fmt"
@@ -6,8 +9,11 @@ func nameMessage() {
   var name string
   fmt.Println("Enter your name")
   fmt.Scanln(&name)
+  //the user's name
   fmt.Println("Hello",name,"welcome to this program! Thank you for taking the time to run this and leave feedback of any sort!")
+  //welcome message
 }
 func main() {
   nameMessage()
+  //recalls the welcome message
 }
